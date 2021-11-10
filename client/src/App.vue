91623a0c4ff8749cb640012d9e78df89a90aeeba
Felipe Paz <xxfelipepaz@gmail.com>
2021-11-10 14:27:28 -0300

    feat: added client project

---
+++ client/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld :msg="msg" />
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld
+  },
+
+  setup() {
+    const msg = ref('Welcome to Your Vue.js Websocket App');
+
+    const websocket = new WebSocket('wss:localhost:3900');
+
+    onMounted(() => {
+      websocket.send('any message');
+    });
+
+    return {
+      msg
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
