ae1d702fa848d2dfebb916d1e75021c23e17780b
Felipe Paz <xxfelipepaz@gmail.com>
2021-11-11 04:39:10 -0300

    feat: added initial send message

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,45 @@
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  <HelloWorld :msg="msg" />
+  <form style="display: block; display: flex; flex-direction: column; gap: 10px; width: 350px; margin: 0 auto;">
+    <input
+      v-model="user"
+      placeholder="Insira um usuário"
+      :readonly="userSelected"
+      :disabled="userSelected"
+    />
+
+    <button
+      @click.stop.prevent="onUserSelected"
+      v-if="!userSelected"
+      :disabled="!user"
+    >Entrar</button>
+
+    <div v-if="userSelected">
+      <textarea
+        v-model="message"
+        placeholder="Envia sua mensagem"
+        ref="msgRef"
+        @keydown.enter.stop.prevent="send"
+      />
+      <button
+        @click.stop.prevent="send"
+        :disabled="!message || message.length < 3"
+      >Send message</button>
+    </div>
+  </form>
+  <div style="display:block;">
+    <HelloWorld
+      :messages="messages"
+      :user="user"
+    />
+
+  </div>
+
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import HelloWorld from './components/HelloWorld';
 
 export default {
@@ -18,16 +52,63 @@ export default {
   },
 
   setup() {
-    const msg = ref('Welcome to Your Vue.js Websocket App');
+    const messages = ref([]);
+    const user = ref(null);
+    const message = ref(null);
+    const socket = ref(null);
+    const msgRef = ref(null);
+    const userSelected = ref(false);
 
-    const websocket = new WebSocket('wss:localhost:3900');
+    const onConnected = () => {
+      socket.value = new WebSocket('ws://localhost:5000/room1');
 
-    onMounted(() => {
-      websocket.send('any message');
-    });
+      socket.value.addEventListener('open', () => console.info('Connected to the server!!!'));
+
+      socket.value.addEventListener('close', () => {
+        socket.value.send(`User ${user.value} has been disconnected.`);
+        console.warn('Disconnected from the server!!!');
+      });
+
+      socket.value.addEventListener('message', ({ data }) => {
+        messages.value.push(JSON.parse(data));
+
+        nextTick(() => {
+          const container = document.querySelector('.msg-container');
+          container.scrollTop = container.scrollHeight + 150;
+        });
+      });
+    };
+
+
+    const send = () => {
+      socket.value.send(
+        JSON.stringify({
+          user: user.value,
+          message: message.value,
+          date: new Date()
+        })
+      );
+      message.value = null;
+      msgRef.value.focus();
+    };
+
+    const onUserSelected = () => {
+      userSelected.value = true;
+      onConnected();
+      nextTick(() => {
+        msgRef.value.focus();
+      });
+    };
 
     return {
-      msg
+      msgRef,
+      messages,
+      message,
+      user,
+      send,
+      onConnected,
+      userSelected,
+      onUserSelected
     };
   }
 }
@@ -41,5 +122,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+form {
+  display: block;
+}
+
+form div {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,40 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="msg-container">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li
+        v-for="(msg, i) in messages"
+        :key="i"
+        :class="msg.user === user ? 'msg-owner': 'msg-user'"
+        class="msg"
+      >
+        <div>
+          {{msg.message}}
+          <small>{{msg.date}}</small>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
+
   props: {
-    msg: String
+    messages: { type: Array, default: () => [] },
+    user: { type: String, default: null }
+  },
+
+  setup() {
   }
 }
 </script>
@@ -46,13 +38,50 @@ h3 {
 }
 ul {
   list-style-type: none;
+  display: flex;
+  flex-direction: column;
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: block;
+  margin: 0 10px 5px;
 }
 a {
   color: #42b983;
+}
+
+.msg-container {
+  max-height: 250px;
+  overflow-y: auto;
+  scrollbar-width: none;
+}
+
+.msg-container::-webkit-scrollbar {
+  width: 0;
+}
+
+.msg {
+  height: 100%;
+  display: block;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.msg small {
+  display: block;
+  font-size: 0.6em;
+  color: #cccccc;
+}
+
+.msg-owner {
+  background-color: red;
+  color: white;
+  text-align: right;
+}
+
+.msg-user {
+  background-color: blue;
+  color: lime;
+  text-align: left;
 }
 </style>
